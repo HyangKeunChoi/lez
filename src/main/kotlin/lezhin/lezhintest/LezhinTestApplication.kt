c8e7fbea1b92d56df6d7ac7df4c0c72c9b357619
HyangKeun Choi <gidrms99@naver.com>
2025-04-12 18:10:33 +0900

    first commit

---
+++ src/main/kotlin/lezhin/lezhintest/LezhinTestApplication.kt
@@ -0,0 +1,11 @@
+package lezhin.lezhintest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LezhinTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<LezhinTestApplication>(*args)
+}
---
+++ src/test/kotlin/lezhin/lezhintest/LezhinTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package lezhin.lezhintest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LezhinTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
